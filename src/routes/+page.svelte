761f10b6944976e065c3a85e53787234dd4558f5
kotiell <tielljustin@gmail.com>
2023-01-22 15:06:16 -0500

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,8 @@
-<div class="container">
-	<h1>Justin Tiell</h1>
-	<div class="main_section">
-		<img src="images/justin-tiell.jpg" alt="Justin Tiell" class="rounded_border"/>
-		<div class="main_section_content">
-			<p>
-				I am an experienced web developer and UX designer with more a decade of working in multiple
-				team environments. I spend time with clients to establish their goals and the needs of the
-				users. Creating mockups and prototypes with Adobe XD allows everyone to get a clear vision
-				of how the solution will look and function. After an agreed upon mockup I then build the
-				app/site with HTML, CSS, JS, and PHP.
-			</p>
-			<p>
-				I am most proficient with frontend languages like HTML, CSS and JS I have also built
-				multiple solutions using React, NextJS, Laravel, and WordPress.
-			</p>
-		</div>
-	</div>
-	<div class="skill_section">
-		<div>
-			<h3>Programming Languages</h3>
-			<div>
-				<ul>
-					<li>HTML</li>
-					<li>CSS/Sass</li>
-					<li>JavaScript</li>
-					<li>PHP</li>
-					<li>React.js</li>
-				</ul>
-			</div>
-		</div>
-		<div>
-			<h3>Skills</h3>
-			<div>
-				<ul>
-					<li>User Experience Design</li>
-					<li>User Research</li>
-					<li>Presenting to Clients</li>
-				</ul>
-			</div>
-		</div>
-		<div>
-			<h3>Tools</h3>
-			<div>
-				<ul>
-					<li>Visual Studio Code</li>
-					<li>Adobe XD</li>
-					<li>Github</li>
-				</ul>
-			</div>
-		</div>
-	</div>
+<div>
+  Justin Tiell
+  <ul>
+    <li>Full Stack Developer</li>
+    <li></li>
+    <li></li>
+  </ul>
 </div>
